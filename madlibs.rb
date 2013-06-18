50cc497db95f28cb9476c99abe1445b506790e2b
tommyb67 <tommybrennan@gmail.com>
2013-06-18 12:21:49 -0400

    first commit for madlibs file

---
+++ madlibs.rb
@@ -0,0 +1,29 @@
+p "Give me a name"
+name = gets.chomp
+
+p "give me an age"
+age = gets.chomp
+
+p "give me a verb"
+first_verb = gets.chomp
+
+p "give me an adverb"
+first_adverb = gets.chomp
+
+p "give me an adjective"
+first_adjective = gets.chomp
+
+p "give me a noun"
+first_noun = gets.chomp
+
+p "give me another verb"
+second_verb = gets.chomp
+
+
+
+
+
+
+
+
+puts "#{name} a #{age} year old #{first_verb} #{first_adverb} at the #{first_adjective} #{first_noun}"
